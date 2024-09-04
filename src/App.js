56acbab700ad711ffaffc44b8bb0f416074521f5
Aayush-Koju <iush.cozu12@gmail.com>
2024-09-04 12:08:18 +0545

    managed nav bar component and added map container in direction.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import NavBar from "./components/NavBar";
+import Direction from "./components/Direction/Direction";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       Map Application
+      <Direction />
     </div>
   );
 }
