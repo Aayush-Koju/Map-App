08a64c1bc91746eefecb1134aeb157cf4e5945e9
Aayush-Koju <iush.cozu12@gmail.com>
2024-09-04 13:53:09 +0545

    added icon on the marker

--- src/components/Direction/Direction.js
+++ src/components/Direction/Direction.js
@@ -1,13 +1,40 @@
 import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "./Direction.css";
+import { Icon } from "leaflet";
 
 function Direction() {
+  const markers = [
+    {
+      geocode: [48.86, 2.3522],
+      popup: "Hello, I am popup 1",
+    },
+    {
+      geocode: [48.85, 2.3522],
+      popup: "Hello, I am popup 2",
+    },
+    {
+      geocode: [48.855, 2.34],
+      popup: "Hello, I am popup 3",
+    },
+  ];
+
+  const customIcon = new Icon({
+    // iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
+    iconUrl: require(`${process.env.PUBLIC_URL}/icons/marker.png`),
+    // iconSize: [38, 38], //size of the icon
+  });
+
   return (
     <MapContainer center={[48.8566, 2.3522]} zoom={13}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {markers.map((marker) => (
+        <Marker position={marker.geocode} icon={customIcon}></Marker>
+      ))}
     </MapContainer>
   );
 }
