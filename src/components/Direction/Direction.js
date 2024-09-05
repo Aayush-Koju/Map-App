72c0bf9c4c7c8eff0a77fcdfd1c030d52e62d805
Aayush-Koju <iush.cozu12@gmail.com>
2024-09-05 13:18:28 +0545

    tried different skins and added satellite image

--- src/components/Direction/Direction.js
+++ src/components/Direction/Direction.js
@@ -2,7 +2,7 @@ import React from "react";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "./Direction.css";
-import { Icon } from "leaflet";
+import { divIcon, Icon, point } from "leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 
 function Direction() {
@@ -27,13 +27,36 @@ function Direction() {
     iconSize: [38, 38], //size of the icon
   });
 
+  const createCustomClusterIcon = (cluster) => {
+    return new divIcon({
+      html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
+      className: "custom-marker-cluster",
+      iconSize: point(33, 33, true),
+    });
+  };
+
   return (
     <MapContainer center={[48.8566, 2.3522]} zoom={13}>
-      <TileLayer
+      {/* <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+      /> */}
+
+      {/* <TileLayer
+        attribution='<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> 
+        | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
+      /> */}
+
+      <TileLayer
+        attribution="&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver"
+        url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png"
       />
-      <MarkerClusterGroup>
+
+      <MarkerClusterGroup
+        chunkedLoading
+        iconCreateFunction={createCustomClusterIcon}
+      >
         {markers.map((marker) => (
           <Marker position={marker.geocode} icon={customIcon}>
             <Popup>{marker.popUp}</Popup>
