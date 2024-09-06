b1b74f70b683c963f185b00aab94a95adfe9f18b
Aayush-Koju <iush.cozu12@gmail.com>
2024-09-06 17:06:02 +0545

    reverse geo coding implemented

--- src/components/NewMarker/NewMarker.js
+++ src/components/NewMarker/NewMarker.js
@@ -1,8 +1,50 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Marker, useMapEvents } from "react-leaflet";
 
 function NewMarker({ customIcon }) {
   const [clickedLocation, setClickedLocation] = useState([]);
+  const [reverseLocation, setReverseLocation] = useState(null);
+
+  useEffect(() => {
+    if (reverseLocation) {
+      console.log(
+        reverseLocation.suburb,
+        reverseLocation.city_district,
+        reverseLocation.city,
+        reverseLocation.municipality,
+        reverseLocation.county
+      );
+    }
+  }, [reverseLocation]);
+
+  useEffect(() => {
+    if (clickedLocation.length > 0) {
+      axios
+        .get(
+          `https://nominatim.openstreetmap.org/reverse?format=json&lat=${Number(
+            clickedLocation[0]
+          )}&lon=${Number(clickedLocation[1])}&zoom=18&addressdetails=1`
+        )
+        .then((response) => {
+          const { address } = response.data;
+
+          const { suburb, city_district, city, municipality, county } = address;
+          setReverseLocation({
+            suburb,
+            city_district,
+            city,
+            municipality,
+            county,
+          });
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+        });
+      // console.log(clickedLocation);
+    }
+  }, [clickedLocation]);
+
   const map = useMapEvents({
     click(e) {
       setClickedLocation([e.latlng.lat, e.latlng.lng]);
