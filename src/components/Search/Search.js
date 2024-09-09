c34150c654e208cca736b2041dec827e5a38d1e3
Aayush-Koju <iush.cozu12@gmail.com>
2024-09-09 13:45:29 +0545

    stored response from api

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Search.css";
+import axios from "axios";
 
 function Search() {
   const [search, setSearch] = useState("");
+  const [result, setResult] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`https://nominatim.openstreetmap.org/search?q=${search}&format=json`)
+      .then((response) => {
+        setResult(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, [search]);
 
   return (
     <div className="search-container">
